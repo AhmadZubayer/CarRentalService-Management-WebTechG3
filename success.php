6896a6a8da42c64024c3a13958780d20428e0fb9
siamshammo <124307695+siamshammo@users.noreply.github.com>
2026-05-18 03:02:02 +0600

    Feature/success23 54736 3 (#16)
    * feat:  Credit Card, bKash, Nagad, Bank Transfer
    
    * feat: successful

---
+++ success.php
@@ -0,0 +1,14 @@
+<?php
+// success.php  (entry point)
+require_once __DIR__ . '/config/auth.php';
+require_once __DIR__ . '/controllers/OrderController.php';
+
+$order_id = (int)($_GET['order_id'] ?? 0);
+if (!$order_id) {
+    header('Location: /index.php');
+    exit();
+}
+
+$controller = new OrderController();
+$controller->showSuccess($order_id);
+?>
